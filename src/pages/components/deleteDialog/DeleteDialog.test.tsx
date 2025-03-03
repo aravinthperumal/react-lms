8321bb4940bc74b7bc006c6efe325ff93ec173b2
Aravinth Perumal <aravinth.perumal@aspiresys.com>
2025-03-03 20:26:41 +0530

    created test cases for dialog and dropdown coponent

---
+++ src/pages/components/deleteDialog/DeleteDialog.test.tsx
@@ -0,0 +1,52 @@
+import {
+  fireEvent,
+  renderWithProviders,
+  screen,
+  waitFor,
+} from "utils/test-utils";
+
+import DeleteDialog from "./DeleteDialog";
+
+describe("render Delete Dialog", () => {
+  const mockOnDeleteFun = jest.fn();
+  const mockOnCloseFun = jest.fn();
+
+  const setup = () => {
+    renderWithProviders(
+      <DeleteDialog
+        title="Delete Dialog"
+        label="Are you want to delete"
+        onDelete={mockOnDeleteFun}
+        onClose={mockOnCloseFun}
+      />,
+    );
+  };
+
+  it("Delete dialog to be in the document", () => {
+    setup();
+    expect(screen.getByText(/Delete Dialog/i)).toBeInTheDocument();
+    expect(screen.getByText(/Are you want to delete/i)).toBeInTheDocument();
+  });
+
+  it("Click on close", async () => {
+    setup();
+    const closeBtn = screen.getByText("Close");
+
+    fireEvent.click(closeBtn);
+
+    await waitFor(() => {
+      expect(mockOnCloseFun).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  it("Click on delete", async () => {
+    setup();
+    const closeBtn = screen.getByText("Delete");
+
+    fireEvent.click(closeBtn);
+
+    await waitFor(() => {
+      expect(mockOnDeleteFun).toHaveBeenCalledTimes(1);
+    });
+  });
+});
---
+++ src/pages/components/dropdown/Dropdown.test.tsx
@@ -0,0 +1,45 @@
+import { fireEvent, renderWithProviders, screen } from "utils/test-utils";
+
+import { Dropdown } from "./Dropdown";
+import { KeyValue } from "./types";
+
+const options: KeyValue[] = [
+  { label: "option1", value: "option1" },
+  { label: "option2", value: "option2" },
+];
+
+describe("Dropdown Component", () => {
+  const onChangeMock = jest.fn();
+
+  const setup = () => {
+    renderWithProviders(
+      <Dropdown
+        name="dropdown"
+        options={options}
+        value="1"
+        onChange={jest.fn()}
+      />,
+    );
+  };
+  it("renders the dropdown with options", () => {
+    setup();
+    expect(screen.getByText("Select an option")).toBeInTheDocument();
+    options.forEach((option) => {
+      expect(screen.getByText(option.label)).toBeInTheDocument();
+    });
+  });
+
+  it("select dropdown in the document", async () => {
+    setup();
+    const select = screen.getByRole("combobox");
+
+    expect(select).toBeInTheDocument();
+  });
+
+  it("does not call onChange when no valid option is selected", () => {
+    setup();
+    fireEvent.change(screen.getByRole("combobox"), { target: { value: "" } });
+
+    expect(onChangeMock).not.toHaveBeenCalled();
+  });
+});
